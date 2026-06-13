1c7e2553d0d8a341c79aa93d8ac64275ce57c233
Jose Santos <js3393@scarletmail.rutgers.edu>
2026-06-13 15:33:35 -0400

    added .gitignore from vs code IDE and also added teacher groups to db

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,19 +1,1 @@
-from fastapi import FastAPI
-from models.student import Student
-
-app = FastAPI()
-student = Student()
-
-@app.get("/")
-def read_root():
-    return {"Successfully": "Created" }
-
-@app.get("/backend/{student_id}")
-def read_item(student_id: int, q: str | None = None):
-    return {"student_id": student_id, "q" : q}
-
-@app.put("/backend/{student_id}")
-def update_student():
-    student1 = 123356
-    return student1
 
